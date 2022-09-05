df2bd48ba45d0ade10974e26f61efc31dda88cd4
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-09-05 00:04:37 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,8 +1,28 @@
-<html>
- <head>
-   <title>Hello World</title>
- </head>
- <body>
-   <?php echo "Hello World!";?>
- </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nova studio</title>
+    <link rel="stylesheet" href="css/nova.css">
+    
+    
+
+</head>
+<body>
+    <header>
+        <nav>
+            <a class="logo" href=""><img src="img/logonova.png" alt=""></a>
+            <ul class="nav-list">
+                <li><a href="">Início</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contatos</a></li>
+                <li><a href="">Entrar</a></li>
+                <li><a href="">Cadastre-se</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main></main>
+</body>
 </html>
