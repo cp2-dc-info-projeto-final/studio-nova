0bcda65e1ecc2206b3c52b7a7f2d71cdc55f417e
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-09-11 00:29:35 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,29 +4,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nova studio</title>
     <link rel="stylesheet" href="css/nova.css">
-    
-    
-
+    <title>Login</title>
 </head>
 <body>
-    <header>
-        <nav>
-            <a class="logo" href=""><img src="img/logonova.png" alt=""></a>
+    <div class="box">
+        <div class="img-box">
+            <img src="img/logo.png" alt="" class="img1">
+        </div>
+    
+        <div class="form-box">
+            <form action="cadastrar.php" method="POST">
 
-            <ul class="nav-list">
-                <li><a href="">Início</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contatos</a></li>
-                <li><a href="">Entrar</a></li>
-                <li><a href="">Cadastre-se</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main></main>
-    <footer>
-        <nav></nav>
-    </footer>
+            <h2>Login</h2>
+            
+            <p>Ainda não possui conta ?</p>
+            <a href="cadastro.php">Clique aqui</a>
+
+            <div class="input-group">
+                <p>Insira seu email</p>
+                <input type="email" name="email" placeholder=" Digite aqui seu e-mail">
+            </div>
+            <div class="input-group">
+                <p>Insira sua senha</p>
+                <input type="email" name="email" placeholder=" Digite aqui sua senha">
+            </div>
+
+            <input type="submit" class="btn" value="Entrar">
+
+        </form>
+    </div>
+</div>
+    
 </body>
 </html>
