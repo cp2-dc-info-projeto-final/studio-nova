89cb206e31ac197ca2e3b051bc8181801bd06019
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-09-28 12:37:21 -0300

    Update novastudio.sql

--- src/novastudio.sql
+++ src/novastudio.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 20-Set-2022 às 01:31
+-- Generation Time: 28-Set-2022 às 16:50
 -- Versão do servidor: 5.7.17
--- PHP Version: 5.6.30
+-- PHP Version: 5.6.
 CREATE DATABASE IF NOT EXISTS novastudio DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE novastudio;
 DROP USER IF EXISTS 'nova'@'localhost';
@@ -24,7 +24,11 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `studionova`
+-- Database: `novastudio`
+--
+
+-- --------------------------------------------------------
+
 --
 -- Estrutura da tabela `administrador`
 --
@@ -35,9 +39,30 @@ CREATE TABLE `administrador` (
   `senha` varchar(200) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-INSERT INTO `administrador` (`id`, `email`,`senha`) VALUES
+--
+-- Extraindo dados da tabela `administrador`
+--
+
+INSERT INTO `administrador` (`id`, `email`, `senha`) VALUES
 (1, 'admin@admin.com', 'adminadmin');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `id_funcionario` int(10) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(200) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `tel` varchar(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
 --
 -- Estrutura da tabela `usuario`
 --
@@ -50,20 +75,21 @@ CREATE TABLE `usuario` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
-
-CREATE TABLE `funcionario` (
-  `id` int(10) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `senha` varchar(200) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 -- Indexes for dumped tables
 --
 
+--
 -- Indexes for table `administrador`
 --
 ALTER TABLE `administrador`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id_funcionario`),
+  ADD UNIQUE KEY `id_funcionario` (`id_funcionario`);
 
 --
 -- Indexes for table `usuario`
@@ -74,17 +100,18 @@ ALTER TABLE `usuario`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-ALTER TABLE `funcionario`
-  ADD PRIMARY KEY (`id`);
+
 --
 -- AUTO_INCREMENT for table `administrador`
 --
 ALTER TABLE `administrador`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
-
+-- AUTO_INCREMENT for table `funcionario`
+--
 ALTER TABLE `funcionario`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_funcionario` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
@@ -93,4 +120,3 @@ ALTER TABLE `usuario`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/* ASDASDASDASD
