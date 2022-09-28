1ba0a6369d57ee82c2178f8c524025283e539f5a
Matheus Da Nova <55743810+MatheusDaNova@users.noreply.github.com>
2022-09-28 12:34:06 -0300

    Update listafun.php

--- src/listafun.php
+++ src/listafun.php
@@ -3,40 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=], initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/nova.css">
-    <title>Lista de clientes</title>
+    <title>Document</title>
 </head>
 <body>
-        <nav>
-            <a class="logo" href="administracao.php"><img src="img/logo.png" alt=""></a>
-            <ul class="nav-list">
-                <h1>Lista de clientes</h1>        
-            </ul>
-        </nav>
-        <main>
-            <?php
-            include ("conecta.php");
+<?php
+    include "conecta.php";
+     
+    
+    if(isset($_GET["id"]) && $_GET["id"] != ""){
+    
+        $id = $_GET["id"];
+        $sql = "DELETE FROM usuario WHERE id = $id;"; 
+        mysqli_query($mysqli,$sql);
+        echo "Cliente excluído com sucesso!<br>";
+        echo "<a href='administracao.php'>Voltar para o início</a><br>";
+        echo "<a href='listacli.php'>Voltar para lista de clientes</a>";
+    } 
 
-                $sql = "SELECT * FROM funcionario;"; 
-                $res = mysqli_query($mysqli,$sql);
-                $linhas = mysqli_num_rows($res);
-                
-                for($i = 0; $i < $linhas; $i++)
-                {
-                    $funcionario = mysqli_fetch_array($res);
-                    echo "<p>Funcionário: ".$funcionario["nome"]."</p>";
-                    echo "<p>E-mail: ".$funcionario["email"]."</p>";
-                    echo "<p>CPF: ".$funcionario["cpf"]."</p>";
-                    echo "<p>Celular: ".$funcionario["tel"]."</p>";
-                    echo "<p><a href='edit.php?id_funcionario=".$funcionario["id_funcionario"]."'>
-                    Editar funcionario</a></p>";
-                    echo "<p><a href='excluir.php?id_funcionario=".$funcionario["id_funcionario"]."'>
-                    Excluir funcionario</a></p>";
-                
-                }
-            
-            ?>
-        </main>
+    if(isset($_GET["id_funcionario"]) && $_GET["id_funcionario"] != ""){
+    
+        $id = $_GET["id_funcionario"];
+        $sql = "DELETE FROM funcionario WHERE id_funcionario = $id;"; 
+        mysqli_query($mysqli,$sql);
+        echo "Cliente excluído com sucesso!<br>";
+        echo "<a href='administracao.php'>Voltar para o início</a><br>";
+        echo "<a href='listacli.php'>Voltar para lista de clientes</a>";
+        }
+?>
 </body>
 </html>
