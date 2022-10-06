040673c70633e18787365f41cea98b54c8d30531
FláviaEduarda <55743819+duda0909@users.noreply.github.com>
2022-10-06 15:42:18 -0300

    Delete src directory

--- src/conecta.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $dbHost = 'localhost';
-    $dbUsername = 'nova';
-    $dbPassword = 'admin';
-    $dbName = 'novastudio';
-    
-    $mysqli = mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName);
-?>
