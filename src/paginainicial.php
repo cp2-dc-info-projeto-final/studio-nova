5460b321f943c2cf443ca803e84494789041ce3c
FláviaEduarda <55743819+duda0909@users.noreply.github.com>
2022-10-19 00:45:01 -0300

    commit 19 10

--- src/paginainicial.php
+++ src/paginainicial.php
@@ -25,7 +25,7 @@
                             <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
                         </svg>
                     </span>
-                    <span class="list">Início</span> 
+                    <span class="title">Início</span> 
                 </a>
             </li>
             <li class="list"></li>
@@ -35,7 +35,7 @@
                             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                         </svg>
                     </span>
-                    <span class="list">Meu perfil</span> 
+                    <span class="title">Meu perfil</span> 
                 </a>
             </li>
             <li class="list"></li>
@@ -45,21 +45,29 @@
                             <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
                         </svg>
                     </span>
-                    <span class="list">Meus agendamentos</span> 
+                    <span class="title">Meus agendamentos</span> 
                 </a>
             </li>
             <li class="list"></li>
-                <a href="logout.php">
+                <a href="#botao-confirma">
                     <span class="icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
                             <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
                         </svg>
                     </span>
-                    <span class="list">Sair</span> 
+                    <span class="title">Sair</span> 
                 </a>
             </li>
         </ul>
+    </div>
+    <div id="botao-confirma" class="confirma">
+        <div class="confirma-conteudo">
+            <h1>Tem certeza que encerrar sua sessão?</h1><br>
+            <a href="logout.php" class="btn1">SIM</a>
+            <a href="" class="btn-cancelar">Cancelar</a>
+
+        </div>
     </div>
 </body>
 </html>
